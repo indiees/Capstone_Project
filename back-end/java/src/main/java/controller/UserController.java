59b948ba456efa4cf546a0bef140d374c6a05c9c
ryan wilson <s3716816@student.rmit.edu.au>
2021-04-01 18:40:20 +1100

    Added user registration

--- back-end/java/src/main/java/controller/UserController.java
+++ back-end/java/src/main/java/controller/UserController.java
@@ -7,6 +7,29 @@ import io.javalin.http.Handler;
 import model.User;
 
 public class UserController {
+    public static Handler createUser = ctx ->{
+        String str_email = ctx.formParam("email");
+        if (str_email==null) {
+            ctx.json(new Status("No 'email' Provided"));
+            return;
+        }
+        String str_password = ctx.formParam("password");
+        if (str_password==null) {
+            ctx.json(new Status("No 'password' Provided"));
+            return;
+        }
+        String str_first_name = ctx.formParam("first_name");
+        if (str_first_name==null) {
+            ctx.json(new Status("No 'first_name' Provided"));
+            return;
+        }
+        String str_last_name = ctx.formParam("last_name");
+        if (str_last_name==null) {
+            ctx.json(new Status("No 'last_name' Provided"));
+            return;
+        }
+    };
+
     public static Handler checkLogin = ctx ->{
         String str_email = ctx.formParam("email");
         if (str_email==null) {
