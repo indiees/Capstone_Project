da92705bd9302519025a79950a4c9d6c1dec3518
s3716883 <s3716883@student.rmit.edu.au>
2021-04-06 15:56:18 +1000

    Added comparison operator

--- back-end/java/src/main/java/model/User.java
+++ back-end/java/src/main/java/model/User.java
@@ -1,8 +1,10 @@
 package model;
 
+import controller.util.Status;
+
 public class User {
     private int user_id;
-    private String email, password, first_name, last_name;
+    public String email, password, first_name, last_name;
     public User(){
 
     }
@@ -29,11 +31,27 @@ public class User {
         return this.last_name;
     }
     public void setUser_id(int user_id) { this.user_id = user_id; }
+    // Careful using the getter, make sure the user actually has an ID
     public int getUser_id(){
        return this.user_id;
     }
     @Override
     public String toString(){
         return this.first_name + " " + this.last_name + " Owned by " + this.email;
+    }
+    @Override
+    public boolean equals(Object o){
+        if (this == o){
+            return true;
+        }
+        if (!(o instanceof User)){
+            return false;
+        }
+        User u = (User) o;
+        return ((u.user_id == this.user_id)
+                && (u.email == this.email)
+                && (u.password == this.password)
+                && (u.first_name == this.first_name)
+                && (u.last_name == this.last_name));
     }
 }
