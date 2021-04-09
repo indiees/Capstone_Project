b32e3c7a9fed29c73ed688105ae9588d189119dd
ryan wilson <s3716816@student.rmit.edu.au>
2021-04-09 18:11:41 +1000

    Adding all the requests for this sprint to web

--- back-end/java/src/main/java/controller/paths/Web.java
+++ back-end/java/src/main/java/controller/paths/Web.java
@@ -4,6 +4,10 @@ public class Web {
     //basic resources
     public static final String checkLogin = "/api/checkLogin";
     public static final String createUser = "/api/user/create";
+    public static final String getBookingsByUser = "/api/user/getBookings";
+    public static final String getBookingsByCar = "/api/car/getBookings";
+    public static final String getCars = "/api/car/search";
+    public static final String getBays = "/api/bays/search";
 
 
 }
