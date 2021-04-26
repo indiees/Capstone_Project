1a1cf00a68bc49cd4540a32ec28fe8c33dbf6227
Musfiqur Rahman <s3703103@student.rmit.edu.au>
2021-04-26 12:10:04 +1000

    added filter section

--- front-end/node/app.js
+++ front-end/node/app.js
@@ -2,12 +2,43 @@ const express = require('express');
 const pug =  require('pug');
 const app = express();
 const port = 80;
+const fetch = require ("node-fetch")
 
 app.get('/', index);
 
 function index(req,res){
     {
         res.send(pug.renderFile("login-register.html"));
+    }
+} 
+
+
+app.get('/bays', bays);
+
+function bays(req,res){
+    {
+        data = [
+            { 
+                "bay_id": 1,
+                "location": "melbourne",
+                "max_capacity": 15
+            },
+            { 
+                "bay_id": 2,
+                "location": "oakleigh",
+                "max_capacity": 20
+            }
+        ] 
+        response=[]
+        url = "http://localhost:7000/api/bay/search"
+        fetch(url)
+            .then(response=>response.json()
+            
+            ) .then(
+                html=> res.send(pug.renderFile("bays.html", data=html.payload))
+            )
+            
+       
     }
 } 
 
