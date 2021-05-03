f62ff896efed251ccba9d5a54921b499a4b766d0
ryan wilson <s3716816@student.rmit.edu.au>
2021-05-03 16:19:54 +1000

    Filter boxes now get formatted into a request which gets sent to the backedn, alkl thats left is to format the resulting data into the cards

--- front-end/node/scripts/car.js
+++ front-end/node/scripts/car.js
@@ -1,3 +1,55 @@
+baseURL = "http://localhost:7000/api/"
 function makeBooking(car_id){
-    console.log("Trying to make booking for car: " + car_id)
+    console.log("Trying to make booking for car: " + car_id + " with user " + localStorage.getItem("email"))
+    formData = {
+                    email:      localStorage.getItem("email"), 
+                    password:   localStorage.getItem("password"),
+                    car_id:     car_id
+                }
+    $.ajax({
+        type: 'POST',
+        url: baseURL + 'booking/create',
+        data: formData,
+        complete: makeBookingCallback
+    });
+}
+
+function makeBookingCallback(data){
+    console.log(data);
+    window.location.replace("/bookings");
+}
+
+$(function() {
+    $('#search').click(function() {
+        //Get all of the fields and store in an easy to access dict
+        data = {
+            cost_min:   $("#min-cost").val(),
+            cost_max:   $("#max-cost").val(),
+            year_min:   $("#min-year").val(),
+            year_max:   $("#max-year").val(),
+            make:       $("#make").val(),
+            color:      $("#color").val(),
+            location:   $("#location").val()
+        }
+        //Format the dict into a string ready to be a param
+        query=""
+        for (key in data){
+            if (data[key]!=""){
+                query+=key+"="+data[key]+"&"
+            }
+        }
+        query=query.substring(0,query.length-1)
+        console.log(query)
+        //send out the request
+        $.ajax({
+            type: 'GET',
+            url: baseURL + 'car/search?' + query,
+            complete: searchCompleteCallback
+        });
+    })
+})
+
+function searchCompleteCallback(data){
+    console.log("Search complete")
+    console.log(data.responseJSON.payload)
 }
