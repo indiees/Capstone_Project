7356ec762d67e4e50fede64d878fe603dbfb8a8c
ryan wilson <s3716816@student.rmit.edu.au>
2021-05-06 16:21:33 +1000

    updated tests to follow the new user accountype attribute

--- back-end/java/src/test/java/dao/UserDAOTest.java
+++ back-end/java/src/test/java/dao/UserDAOTest.java
@@ -16,11 +16,12 @@ public class UserDAOTest {
     private static final String FNAME = "UserDAOTest.first.name";
     private static final String LNAME = "UserDAOTest.last.name";
     private static final String DUMMY = "testMissingValue";
+    private static final int ACCTYPE = 1;
 
     @BeforeEach
     public void setup() {
         if (!userExists) {
-            testUser = UserDAO.createUser(EMAIL, PASS, FNAME, LNAME);
+            testUser = UserDAO.createUser(EMAIL, PASS, FNAME, LNAME, ACCTYPE);
             userExists = (testUser != null);
         }
     }
