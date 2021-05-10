75170eb505069c254290410104417e94d089b38f
ryan wilson <s3716816@student.rmit.edu.au>
2021-05-10 19:47:01 +1000

    Added callback functions for the three pages

--- front-end/node/scripts/admin.js
+++ front-end/node/scripts/admin.js
@@ -7,43 +7,48 @@ $(document).on("click", ".adminNav button", function(data){
     show(data.target.getAttribute("tg"))
 });
 
+const baseURL = "http://localhost:7000/api/";
+
 $(document).on("click", "#carBtn", function(data){
     console.log("refreshing cars page")
-    $("#carLoading").show();/*
-    formData = {email:      $("#loginEmail").val(), 
-                password:   $("#loginPassword").val()}
+    $("#carLoading").show();
     $.ajax({
-        type: 'POST',
-        url: baseURL + 'checkLogin',
-        data: formData,
-        complete: loginCallback
-    });*/
+        type: 'GET',
+        url: baseURL + 'car/search',
+        complete: carCallback
+    });
 });
+
+function carCallback(){
+    $("#carLoading").hide();
+}
 
 $(document).on("click", "#userBtn", function(data){
     console.log("refreshing user page")
-    $("#userLoading").show();/*
+    $("#userLoading").show();
     formData = {email:      $("#loginEmail").val(), 
                 password:   $("#loginPassword").val()}
     $.ajax({
-        type: 'POST',
-        url: baseURL + 'checkLogin',
+        type: 'GET',
+        url: baseURL + 'car/search',
         data: formData,
-        complete: loginCallback
-    });*/
+        complete: userCallback
+    });
 });
-
+function userCallback(){
+    $("#userLoading").hide();
+}
 
 $(document).on("click", "#bayBtn", function(data){
     console.log("refreshing bay page")
-    $("#bayLoading").show();/*
-    formData = {email:      $("#loginEmail").val(), 
-                password:   $("#loginPassword").val()}
+    $("#bayLoading").show();
     $.ajax({
-        type: 'POST',
-        url: baseURL + 'checkLogin',
-        data: formData,
-        complete: loginCallback
-    });*/
+        type: 'GET',
+        url: baseURL + 'bay/search',
+        complete: bayCallback
+    });
 });
 
+function bayCallback(){
+    $("#bayLoading").hide();
+}
