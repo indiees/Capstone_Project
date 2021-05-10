747f8a6e5c2a7e0e651de48651972755fbd9eef9
ryan wilson <s3716816@student.rmit.edu.au>
2021-05-10 19:40:52 +1000

    Added loading screens

--- front-end/node/scripts/admin.js
+++ front-end/node/scripts/admin.js
@@ -3,7 +3,47 @@ function show(div){
     $("#" + div).show();
 };
 
-
 $(document).on("click", ".adminNav button", function(data){
     show(data.target.getAttribute("tg"))
 });
+
+$(document).on("click", "#carBtn", function(data){
+    console.log("refreshing cars page")
+    $("#carLoading").show();/*
+    formData = {email:      $("#loginEmail").val(), 
+                password:   $("#loginPassword").val()}
+    $.ajax({
+        type: 'POST',
+        url: baseURL + 'checkLogin',
+        data: formData,
+        complete: loginCallback
+    });*/
+});
+
+$(document).on("click", "#userBtn", function(data){
+    console.log("refreshing user page")
+    $("#userLoading").show();/*
+    formData = {email:      $("#loginEmail").val(), 
+                password:   $("#loginPassword").val()}
+    $.ajax({
+        type: 'POST',
+        url: baseURL + 'checkLogin',
+        data: formData,
+        complete: loginCallback
+    });*/
+});
+
+
+$(document).on("click", "#bayBtn", function(data){
+    console.log("refreshing bay page")
+    $("#bayLoading").show();/*
+    formData = {email:      $("#loginEmail").val(), 
+                password:   $("#loginPassword").val()}
+    $.ajax({
+        type: 'POST',
+        url: baseURL + 'checkLogin',
+        data: formData,
+        complete: loginCallback
+    });*/
+});
+
