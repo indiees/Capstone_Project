38ae0435237b9b72c08d7325fa3d72755957edbc
ryan wilson <s3716816@student.rmit.edu.au>
2021-05-10 20:58:10 +1000

    Changed bay path to path other entities

--- back-end/java/src/main/java/controller/paths/Web.java
+++ back-end/java/src/main/java/controller/paths/Web.java
@@ -16,6 +16,6 @@ public class Web {
     public static final String editCar = "/api/car/update";
     public static final String addCar = "/api/car/create";
     public static final String editBay = "/api/bay/update";
-    public static final String addBay = "/api/bay/add";
+    public static final String addBay = "/api/bay/create";
 
 }
